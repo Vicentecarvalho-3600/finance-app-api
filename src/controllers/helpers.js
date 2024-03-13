fe0072ddca52c30c0b470a70e0c2fcf703febaf0
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-03-13 15:16:57 -0300

    fix helpersControllers

--- src/controllers/helpers.js
+++ src/controllers/helpers.js
@@ -2,14 +2,12 @@ export const badRequest = (body) => ({ statusCode: 400, body })
 
 export const created = (body) => ({ statusCode: 201, body })
 
-export const serverError = () => {
-    return {
-        statusCode: 500,
-        body: {
-            message: "Internal server error",
-        },
-    }
-}
+export const serverError = () => ({
+    statusCode: 500,
+    body: {
+        message: "Internal server error",
+    },
+})
 
 export const ok = (body) => ({ statusCode: 200, body })
 
