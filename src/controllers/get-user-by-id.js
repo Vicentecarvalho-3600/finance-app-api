cbc07ffc9b5361a45620af6f4f9e4bbc8ea2602f
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-03-13 16:15:40 -0300

    feat: add helper functions to user controllers

--- src/controllers/get-user-by-id.js
+++ src/controllers/get-user-by-id.js
@@ -1,5 +1,6 @@
-import { serverError, ok, badRequest, notfound } from "./helpers.js"
+import { serverError, ok, notfound } from "./helpers/http.js"
 import { GetUserByIdUseCase } from "../use-cases/get-user-by-id.js"
+import { invalidIdResponse } from "./helpers/user.js"
 import validator from "validator"
 
 export class GetUserByIdController {
@@ -8,9 +9,7 @@ export class GetUserByIdController {
             const isIdValid = validator.isUUID(httpRequest.params.userId)
 
             if (!isIdValid) {
-                return badRequest({
-                    message: "The provided id is not valid",
-                })
+                return invalidIdResponse()
             }
 
             const getUserByIdUseCase = new GetUserByIdUseCase()
