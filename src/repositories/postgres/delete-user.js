51c99d7766044dfde2b8edd3e6c7c5d0b97d69a3
Vicentecarvalho-3600 <vicentecarvalho.cooknet@gmail.com>
2024-03-14 21:16:43 -0300

    fix: PostgresDeleteUserRepository

--- src/repositories/postgres/delete-user.js
+++ src/repositories/postgres/delete-user.js
@@ -1,6 +1,6 @@
 import { PostgresHelper } from "../../db/postgres/helper"
 
-export class PostgresdeleteUser {
+export class PostgresDeleteUserRepository {
     async execute(userId) {
         const deletedUser = await PostgresHelper.query(
             "DELETE FROM users WHERE id = $1 RETURNIG *",
